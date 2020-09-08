d7ad43501cf0b3385019d8bd49cced421cd18def
honorashka <honor353@yandex.ru>
2020-09-09 00:26:48 +0300

    1. Правки + Б30

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,7 @@
   var navHeader = document.querySelector('.nav');
   var navToggle = document.querySelector('.nav__toggle');
 
-  navHeader.classList.remove('main-nav--nojs');
+  navHeader.classList.remove('nav--nojs');
 
   navToggle.addEventListener('click', function() {
     if (navHeader.classList.contains('nav--closed')) {
